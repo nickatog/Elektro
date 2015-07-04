2d6e7c58b9a32565af411e5f396cecc733e09964
Nicholas Anderson <nickatog@gmail.com>
2015-07-04 00:01:40 -0500

    Just fucking around.

--- playerController.js
+++ playerController.js
@@ -2,4 +2,34 @@ angular
 .module('elektroApp', [])
 .controller('playerController',
 function ($scope) {
+	$scope.players = [];
+	
+	$scope.addPlayerFormButtonHide = false;
+	$scope.addPlayerFormVisible = false;
+	
+	$scope.newPlayerName = '';
+	
+	$scope.toggleAddPlayerForm =
+		function() {
+			$scope.addPlayerFormButtonHide = true;
+			$scope.addPlayerFormVisible = true;
+		};
+	
+	$scope.addPlayer =
+		function() {
+			$scope.players.push({ name: $scope.newPlayerName, money: 0 });
+			
+			$scope.addPlayerFormButtonHide = false;
+			$scope.addPlayerFormVisible = false;
+			
+			$scope.newPlayerName = '';
+		};
+	
+	$scope.addPlayerCancel =
+		function() {
+			$scope.addPlayerFormButtonHide = false;
+			$scope.addPlayerFormVisible = false;
+			
+			$scope.newPlayerName = '';
+		};
 });
